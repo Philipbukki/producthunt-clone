861502bf7c36e7a79eafefc58c2bd4d26f33388f
Philip Bukki <philipbukkie@gmail.com>
2021-08-06 17:41:04 +0300

    cchanged db to sqlite3

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'producthuntdb',
-        'PASSWORD': 'password',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
